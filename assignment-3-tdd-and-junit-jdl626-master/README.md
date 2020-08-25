2c22481d24fe679833c3654ba42d9c509f03f0b6
luckjd-7 <60764053+luckjd-7@users.noreply.github.com>
2020-08-25 00:57:26 -0400

    Add files via upload

---
+++ assignment-3-tdd-and-junit-jdl626-master/README.md
@@ -0,0 +1,3 @@
+# Assignment 3
+Please check Blackboard for the specifications and rubric for this assignment.
+The starter code created in class will be uploaded here.
---
+++ assignment-3-tdd-and-junit-jdl626-master/src/FractionalNumberDriver.java
@@ -0,0 +1,16 @@
+/**
+ * Auto Generated Java Class.
+ */
+public class FractionalNumberDriver {
+  
+  
+  public static void main(String[] args) { 
+    FractionalNumber num1 = new FractionalNumber(1/2);
+    FractionalNumber num2 = new FractionalNumber(1/4);
+    num1.add(num2);
+    System.out.println(num1);
+  }
+  
+  /* ADD YOUR CODE HERE */
+  
+}
